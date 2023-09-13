54afa79d12eb3361354e30631e8fa08c8977fbde
KuroXI <vgonzales5157@gmail.com>
2023-09-13 21:44:28 +0800

    Edit the answer

--- Second Year/IM - LAB Assignment 2.sql
+++ Second Year/IM - LAB Assignment 2.sql
@@ -10,11 +10,12 @@ SELECT * FROM product;
 
 ALTER TABLE product ADD CONSTRAINT unit_ck CHECK (unit IN ('pc', 'ea', 'pkg', 'mtr', 'Ltr'));
 
-INSERT INTO product VALUES ('PS0003', 'Cisco Virt Hardware', 'pcs');
-INSERT INTO product VALUES ('PC0002', 'Dell 745 Opti Desk', 'pg');
-INSERT INTO product VALUES ('PA0001', 'MS Ofc Business 2013', 'pk');
-INSERT INTO product VALUES ('AM0001', 'MS Wireless Mouse', 'pcs');
-INSERT INTO product VALUES ('AD0001', 'Toshiba Canvio 1 TB', 'eac');
+INSERT INTO product VALUES
+	('PS0003', 'Cisco Virt Hardware', 'pcs'),
+	('PC0002', 'Dell 745 Opti Desk', 'pg'),
+	('PA0001', 'MS Ofc Business 2013', 'pk'),
+	('AM0001', 'MS Wireless Mouse', 'pcs'),
+	('AD0001', 'Toshiba Canvio 1 TB', 'eac');
 
 CREATE TABLE pricehist(
 	effDate DATE NOT NULL,
@@ -27,11 +28,13 @@ CREATE TABLE pricehist(
 
 CALL SYSPROC.ADMIN_CMD('DESCRIBE TABLE pricehist');
 
-INSERT INTO pricehist VALUES ('2010-05-15', 'NB0003', '199');
-INSERT INTO pricehist VALUES ('2010-05-15', 'NB0004', '279');
-INSERT INTO pricehist VALUES ('2010-05-15', 'NB0005', '350');
+INSERT INTO pricehist VALUES
+	('2010-05-15', 'NB0003', '199'),
+	('2010-05-15', 'NB0004', '279'),
+	('2010-05-15', 'NB0005', '350');
 
 SELECT * FROM pricehist;
 
 INSERT INTO pricehist VALUES ('2011-02-01', 'PS0003', '123.55');
+
 INSERT INTO pricehist VALUES ('2011-02-01', 'NB0005', '-1.85');
