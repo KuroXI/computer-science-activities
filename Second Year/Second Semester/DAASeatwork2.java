6bc0d8fa8288be9d9b13fb47884f9ef7fdd4d412
Victor Gonzales <vgonzales5157@gmail.com>
2024-02-14 13:44:59 +0000

    change it to menu list app

--- Second Year/Second Semester/DAASeatwork2.java
+++ Second Year/Second Semester/DAASeatwork2.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class DAASeatwork2 {
   public static void main(String[] args) {
-    int number = 15;
-    fibonacciRecursive(number, 0, 1);
-    System.out.println();
-    fibonacciNonRecursive(number);
-    System.out.println();
+    boolean hasStopped = false;
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter value: ");
+    int number = scanner.nextInt();
+
+    do {
+      System.out.println("\na. Recursive");
+      System.out.println("b. Non-Recursive");
+      System.out.println("c. Exit");
+      System.out.print("Choose method: ");
+      String method = scanner.next();
+
+      switch (method.toLowerCase()) {
+        case "a":
+          fibonacciRecursive(number, 0, 1);
+          System.out.println();
+          break;
+        case "b":
+          fibonacciNonRecursive(number);
+          System.out.println();
+          break;
+        case "c":
+          hasStopped = true;
+          System.out.println("John Victor H. Gonzales.");
+          break;
+        default:
+          System.out.println("Invalid method");
+      }
+    } while (!hasStopped);
+
+    scanner.close();
   }
 
   private static void fibonacciRecursive(int equal, int first, int second) {
-    if (equal < 1) return;
+    if (equal < 1)
+      return;
 
     System.out.print(first + " ");
     fibonacciRecursive(equal - 1, second, first + second);
